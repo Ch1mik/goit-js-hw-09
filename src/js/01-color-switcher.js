56784ab3e6d1089540ecbb4297f16e94669deae6
Ch1mik <125959505+Ch1mik@users.noreply.github.com>
2023-05-10 09:45:18 +0200

    done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,43 @@
+const refs = {
+  start_btn: document.querySelector('[data-start]'),
+  stop_btn: document.querySelector('[data-stop]'),
+};
+refs.start_btn.addEventListener('click', startChange);
+refs.stop_btn.addEventListener('click', stopChange);
+let intervalId;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function changeBackgroundColor() {
+  document.body.style.background = getRandomHexColor();
+}
+
+function startChange() {
+  refs.start_btn.disabled = true;
+  intervalId = setInterval(changeBackgroundColor, 500);
+}
+
+function stopChange() {
+  refs.start_btn.disabled = false;
+  clearInterval(intervalId);
+}
+
+refs.start_btn.style.display = 'flex';
+refs.start_btn.style.justifyContent = 'center';
+refs.start_btn.style.width = '100px';
+refs.start_btn.style.fontSize = '30px';
+refs.start_btn.style.position = 'absolute';
+refs.start_btn.style.top = '50%';
+refs.start_btn.style.left = '50%';
+
+refs.stop_btn.style.display = 'flex';
+refs.stop_btn.style.justifyContent = 'center';
+refs.stop_btn.style.width = '100px';
+refs.stop_btn.style.fontSize = '30px';
+refs.stop_btn.style.position = 'absolute';
+refs.stop_btn.style.top = '50%';
+refs.stop_btn.style.left = '40%';
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import Notiflix from 'notiflix';
+const formElement = document.querySelector('form');
+const delayElement = formElement.querySelector('input[ name="delay"]');
+const stepElement = formElement.querySelector('input[name="step"]');
+const amountElement = formElement.querySelector('input[name="amount"]');
+const create_btn = formElement.querySelector('button[type="submit"]');
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
 }
+create_btn.addEventListener('click', e => {
+  e.preventDefault();
+  let firstDelay = Number(delayElement.value);
+  let delayStep = Number(stepElement.value);
+  let promiseAmount = Number(amountElement.value);
+  for (let i = 0; i < promiseAmount; i++) {
+    const delay = firstDelay + i * delayStep;
+    const position = i + 1;
+
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+});
